ac0b504ab11115d4e60ab9474227c1c64a944dc3
Shanmukh <sanmukswamy238@gmail.com>
2022-02-08 13:10:12 +0530

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:'addemployee',component:AddEmployeeComponent}
+  {path:'addemployee',component:AddEmployeeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent}
   
 ];
 
